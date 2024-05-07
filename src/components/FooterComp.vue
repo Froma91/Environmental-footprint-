fcb1c7f1743210f3f47008dfcb14937f0c7edaf9
froma91 <fredyrodriguez91@hotmail.com>
2024-05-07 23:42:58 +0200

    créatio du footer

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,18 @@
+<template>
+  <hr class="featurette-divider" />
+  <footer class="text-muted py-5">
+    <div class="container">
+      <p class="float-end mb-1">
+        <a href="#">Back to top</a>
+      </p>
+      <p class="mb-1">
+        Cette page web a été realisé par un étudiant de la formation informatique de gestion à la
+        HEG-IG
+      </p>
+      <p class="mb-0">
+        Agit maintenant pour la protection de l'environnement, un click fait la différence !
+        <a href="https://www.he-arc.ch/" target="_blank">Institution</a>
+      </p>
+    </div>
+  </footer>
+</template>
