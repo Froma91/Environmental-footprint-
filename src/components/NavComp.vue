1f9a9ae6846dfe51e6c255272b7d633eb79bdd9e
froma91 <fredyrodriguez91@hotmail.com>
2024-05-07 23:42:45 +0200

    création de la navbar

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -1,18 +1,14 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-success">
     <div class="container justify-content-around">
-      <a class="navbar-brand">GREEN-IT</a>
+      <router-link to="/" class="navbar-brand text-light fw-bold">Eco-web</router-link>
       <router-link to="/" class="nav-link active" aria-current="page">
-        <a class="nav-link active">Projet</a>
+        <a class="text-black nav-link active fs-5">Empreinte écologique</a>
       </router-link>
 
-      <router-link to="/empreinte" class="nav-link"><a class="nav-link">Empreinte</a> </router-link>
-
-      <router-link to="/maturite" class="nav-link"
-        ><a class="nav-link">Maturité éco-conception</a>
+      <router-link to="/empreinte" class="nav-link"
+        ><a class="text-black nav-link fs-5">Informations</a>
       </router-link>
-
-      <router-link to="/about" class="nav-link"><a class="nav-link">Participants</a> </router-link>
     </div>
   </nav>
 </template>
